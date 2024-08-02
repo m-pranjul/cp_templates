9304271699a885d02b2748c22936abb978469392
pranjul_m <pranjulmalpani@gmail.com>
2024-08-02 22:41:22 +0530

    array added in linked list node

--- linked_list.py
+++ linked_list.py
@@ -6,3 +6,9 @@ class Node:
 class deque:
     def __init__(self) -> None:
         pass
+
+class array:
+    def __init__(self,arr) -> None:
+        self.arr=arr
+    def append(self,val):
+        self.arr.append(val)
