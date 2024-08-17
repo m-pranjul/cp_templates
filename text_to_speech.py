db65935c4b074f98095140da74acbdcba76a0687
m_pranjul <pranjulmalpani@gmail.com>
2024-08-17 12:22:19 +0530

    Added multiple voices in text-to-speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,6 +2,19 @@ import win32com.client
 
 say = win32com.client.Dispatch("SAPI.SpVoice")
 
-speech = input()
+# speech = input()
+speech = "Hola Sir, buenos dias, mucho gusto"
 
-say.speak(speech)
+voices = say.GetVoices()
+say.speak(speech) # David Voice
+
+say.Voice = voices.Item(1)
+
+say.speak(speech) # Zira Voice
+
+
+
+# Can check for available voices
+# for i , voice in enumerate(voices):
+#     print(f"Voice {i+1}: {voice.GetDescription()}")
+
